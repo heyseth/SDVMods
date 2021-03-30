e6dbcbf971ee35641a9776d3e20f1703a08c4cde
heyseth <sethmillerp@gmail.com>
2021-03-30 11:57:17 -0700

    Initial release

---
+++ heysethSitPause/heysethSitPause/ModEntry.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace heysethSitPause
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+        }
+
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            foreach (var player in Game1.getAllFarmers())
+            {
+                if (player.IsSitting())
+                {
+                    Game1.gameTimeInterval = 0;
+                }
+            }
+        }
+    }
+}
---
+++ heysethUmbrellas/heysethUmbrellas/UmbrellaConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heysethUmbrellas
+{
+    public class UmbrellaConfig
+    {
+        public bool enableWetness = false;
+        public float staminaDrainRate = 2.0f;
+    }
+}
